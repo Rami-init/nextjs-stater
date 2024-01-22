65ff9e597bfbcf8d76f7b95bc4c190514da94d7f
ramyradwan <rradwan@sequenttal.com>
2024-01-22 20:02:09 +0200

    add husky & lint-staged

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: "none",
+};
+
+module.exports = config;
